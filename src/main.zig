7236a7938748f796cd1ef8a2d77d070f8f69eb79
tovedetered <tobedetered@gmail.com>
2024-05-07 13:09:10 -0400

    Step 5

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 const io = @import("std").io;
+const posix = @import("std").posix;
+const os = @import("std").os;
+const c = @import("std").c;
 
 pub fn main() !void {
+    try enableRawMode();
     const stdin = io.getStdIn().reader();
     var buf: [1]u8 = undefined;
 
@@ -10,4 +14,12 @@ pub fn main() !void {
         if (n != 1) break;
         if(buf[0] == 'q') break;
     }
+}
+
+fn enableRawMode() !void{
+    const fd = io.getStdIn().handle;
+    var raw = try posix.tcgetattr(fd);
+    raw.lflag.ECHO = false;
+
+    try posix.tcsetattr(fd, .FLUSH, raw);
 }
