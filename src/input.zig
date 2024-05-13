775c6de012112a98a0d1050fa0c22b0da0619c24
tovedetered <tobedetered@gmail.com>
2024-05-13 08:45:19 -0400

    step 79

--- src/input.zig
+++ src/input.zig
@@ -10,57 +10,79 @@ const KeyAction = enum {
     Quit,
     NoOp,
     MoveCursor,
-};
+    };
 
 pub fn editorProcessKeyPress() !KeyAction {
     const c = try term.editorReadKey();
     return switch (c) {
-        util.ctrlKey('q') => .Quit,
-        @intFromEnum(data.editorKey.ARROW_LEFT),
-        @intFromEnum(data.editorKey.ARROW_RIGHT),
-        @intFromEnum(data.editorKey.ARROW_UP),
-        @intFromEnum(data.editorKey.ARROW_DOWN),
-        => {
-            editorMoveCursor(c);
-            return .NoOp;
-        },
-        @intFromEnum(data.editorKey.PAGE_UP), @intFromEnum(data.editorKey.PAGE_DOWN) => {
-            var times = data.editor.screenRows;
-            while (times > 0) : (times -= 1) {
-                if (c == @intFromEnum(data.editorKey.PAGE_UP)) {
-                    editorMoveCursor(@intFromEnum(data.editorKey.ARROW_UP));
-                } else {
-                    editorMoveCursor(@intFromEnum(data.editorKey.ARROW_DOWN));
-                }
+    util.ctrlKey('q') => .Quit,
+    @intFromEnum(data.editorKey.ARROW_LEFT),
+    @intFromEnum(data.editorKey.ARROW_RIGHT),
+    @intFromEnum(data.editorKey.ARROW_UP),
+    @intFromEnum(data.editorKey.ARROW_DOWN),
+    => {
+        editorMoveCursor(c);
+        return .NoOp;
+    },
+    @intFromEnum(data.editorKey.PAGE_UP), @intFromEnum(data.editorKey.PAGE_DOWN) => {
+        var times = data.editor.screenRows;
+        while (times > 0) : (times -= 1) {
+            if (c == @intFromEnum(data.editorKey.PAGE_UP)) {
+                editorMoveCursor(@intFromEnum(data.editorKey.ARROW_UP));
+            } else {
+                editorMoveCursor(@intFromEnum(data.editorKey.ARROW_DOWN));
             }
-            return .NoOp;
-        },
-        @intFromEnum(data.editorKey.HOME_KEY) => {
-            data.input.cx = 0;
-            return .NoOp;
-        },
-        @intFromEnum(data.editorKey.END_KEY) => {
-            data.input.cx = data.editor.screenCols - 1;
-            return .NoOp;
-        },
-        else => .NoOp,
+        }
+        return .NoOp;
+    },
+    @intFromEnum(data.editorKey.HOME_KEY) => {
+        data.input.cx = 0;
+        return .NoOp;
+    },
+    @intFromEnum(data.editorKey.END_KEY) => {
+        data.input.cx = data.editor.screenCols - 1;
+        return .NoOp;
+    },
+    else => .NoOp,
     };
 }
 
 pub fn editorMoveCursor(key: u16) void {
+    var row:?*data.erow =
+    if (data.input.cy >= data.editor.numRows or data.editor.row.len == 0) null
+    else &data.editor.row[data.input.cy];
+
     switch (key) {
-        @intFromEnum(data.editorKey.ARROW_LEFT) => {
-            if (data.input.cx > 0) {
-                data.input.cx -= 1;
-            } else {}
-        },
-        @intFromEnum(data.editorKey.ARROW_RIGHT) => data.input.cx += 1,
-        @intFromEnum(data.editorKey.ARROW_UP) => if (data.input.cy > 0) {
+    @intFromEnum(data.editorKey.ARROW_LEFT) => {
+        if (data.input.cx > 0) {
+            data.input.cx -= 1;
+        } else if(data.input.cy > 0) {
             data.input.cy -= 1;
-        },
-        @intFromEnum(data.editorKey.ARROW_DOWN) => if (data.input.cy < data.editor.numRows) {
-            data.input.cy += 1;
-        },
-        else => unreachable,
+            data.input.cx = @as(u16, @intCast(data.editor.row[data.input.cy].chars.len));
+        }
+    },
+    @intFromEnum(data.editorKey.ARROW_RIGHT) =>{
+        if(row != null){
+            if(data.input.cx < row.?.*.chars.len){
+                data.input.cx += 1;
+            } else if(data.input.cx >= data.editor.row.len){
+                data.input.cy += 1;
+                data.input.cx = 0;
+            }
+        }
+    },
+    @intFromEnum(data.editorKey.ARROW_UP) => if (data.input.cy > 0) {
+        data.input.cy -= 1;
+    },
+    @intFromEnum(data.editorKey.ARROW_DOWN) => if (data.input.cy < data.editor.numRows) {
+        data.input.cy += 1;
+    },
+    else => unreachable,
+    }
+
+    row = if (data.input.cy >= data.editor.numRows) null else &data.editor.row[data.input.cy];
+    const rowlen = if(row != null) row.?.chars.len else 0;
+    if (data.input.cx > rowlen) {
+        data.input.cx = @as(u16, @intCast(rowlen));
     }
 }
