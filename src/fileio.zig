21b6d3ab3983278b4da00f58e06c098567484dc7
tovedetered <tobedetered@gmail.com>
2024-05-14 10:06:41 -0400

    step 105

--- src/fileio.zig
+++ src/fileio.zig
@@ -3,6 +3,25 @@ const io = @import("std").io;
 const data = @import("./data.zig");
 const row = @import("./rowOps.zig");
 
+pub fn editorRowsToString() ![]u8{
+    var totlen: usize = 0;
+    for(0..data.editor.numRows) |j|{
+        totlen += data.editor.row[j].chars.len + 1;
+    }
+
+    var buffer = try data.editor.ally.alloc(u8, totlen);
+    var p: usize = 0;
+    for(0..data.editor.numRows) |j| {
+        @memcpy(buffer[p..p+data.editor.row[j].chars.len],
+            data.editor.row[j].chars);
+        p += data.editor.row[j].chars.len;
+        buffer[p] = '\n';
+        p += 1;
+    }
+
+    return buffer;
+}
+
 pub fn editorOpen(filename: []const u8) !void {
     data.editor.ally.free(data.editor.filename);
     data.editor.filename = try data.editor.ally.alloc(u8, filename.len);
