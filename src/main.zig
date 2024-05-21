50795f1aa7893772810f7772d8b98e87f8a43ab9
tovedetered <tobedetered@gmail.com>
2024-05-21 19:39:00 -0400

    enabled logging to a file

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,11 @@ const data = @import("./data.zig");
 const cont = @import("./fileio.zig");
 const output = @import("./output.zig");
 
+pub const std_options = .{
+    .log_level = .debug,
+    .logFn = logToFile,
+};
+
 pub fn main() !void {
     try terminal.enableRawMode();
     defer terminal.disableRawMode();
@@ -27,18 +32,18 @@ pub fn main() !void {
     if (args.len > 1) {
         try cont.editorOpen(args[1]);
     }
-
+    
     try output.editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit", .{});
 
     while (true) {
         try out.editorRefreshScreen();
         const op = try input.editorProcessKeyPress();
         switch (op) {
-        .Quit => {
-            try out.editorRefreshScreen();
-            break;
-        },
-        else => {},
+            .Quit => {
+                try out.editorRefreshScreen();
+                break;
+            },
+            else => {},
         }
     }
 }
@@ -51,6 +56,40 @@ pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, n: ?
     std.builtin.default_panic(msg, error_return_trace, n);
 }
 
+pub fn logToFile(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const scope_prefix = "(" ++ switch (scope) {
+        std.log.default_log_scope => @tagName(scope),
+        else => if (@intFromEnum(level) <= @intFromEnum(std.log.Level.err))
+            @tagName(scope)
+        else
+            return,
+    } ++ "): ";
+
+    const prefix = "[" ++ comptime level.asText() ++ "] " ++ scope_prefix;
+
+    const dirname = std.fs.getAppDataDir(std.heap.page_allocator, "sleep") catch {
+        std.debug.print("Failed at getting the data dir\n", .{});
+        return;
+    };
+    var dir = std.fs.openDirAbsolute(dirname, .{}) catch {
+        std.debug.print("Failed opening dir: {s}\n", .{dirname});
+        return;
+    };
+    var file: std.fs.File = dir.openFile("log", .{ .mode = .write_only }) catch |err| switch(err){
+        std.fs.Dir.OpenError.FileNotFound => dir.createFile("log", .{}) catch return,
+        else => return,
+    };
+    defer file.close();
+    file.seekTo(file.getEndPos() catch return) catch return;
+    const message = std.fmt.allocPrint(std.heap.page_allocator, prefix ++ format ++ "\n", args) catch return;
+    file.writeAll(message) catch return;
+}
+
 fn initEditor(alloc: std.mem.Allocator) !void {
     data.editor.ally = alloc;
     data.input.cx = 0;
@@ -60,7 +99,9 @@ fn initEditor(alloc: std.mem.Allocator) !void {
     data.editor.rowoff = 0;
     data.editor.coloff = 0;
     data.editor.row = &.{};
+
     try terminal.getWindowSize(&data.editor.screenRows, &data.editor.screenCols);
+
     data.editor.screenRows -= 2;
     data.editor.statustime = 0;
 }
